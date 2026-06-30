a3a9a5d5f9221923aa5e2a5a9bd58c23b73378ca
webcolorspl <webcolors.pl@gmail.com>
2026-06-30 15:48:12 +0200

    fix: /creator redirect bezpośrednio do ad-generator

--- src/app/creator/page.tsx
+++ src/app/creator/page.tsx
@@ -1,13 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function CreatorPage() {
-  return (
-    <iframe
-      src="https://ad-generator-webcolors.vercel.app"
-      style={{
-        position: 'fixed', inset: 0,
-        width: '100%', height: '100%',
-        border: 'none',
-      }}
-      allow="fullscreen"
-    />
-  )
+  redirect('https://ad-generator-webcolors.vercel.app')
 }
